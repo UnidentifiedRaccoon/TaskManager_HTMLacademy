f6454c00358918b969f6f4ff01fc05e2b621ed1d
UnidentifiedRaccoon <yura.posledov@yandex.ru>
2022-07-14 23:39:03 +0300

    7.2.2 Добавит методы скрытия/показа

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -71,6 +71,14 @@ export default class BoardController {
     this._tasksModel.setFilterChangeHandler(this._onFilterChange);
   }
 
+  hide() {
+    this._container.hide();
+  }
+
+  show() {
+    this._container.show();
+  }
+
   render() {
     const isAllTasksArchived = this._tasksModel.getTasksData().every((task) => task.isArchive);
     const isNoTasks = this._tasksModel.getTasksData().length === 0;
